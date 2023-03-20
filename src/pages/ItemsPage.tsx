077d8b9bb5e6be204f871fe63a144383d212c33e
Rcong <zcr0903@gmail.com>
2023-03-21 00:00:47 +0800

    侧边栏滑动

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { AddItemFloatButton } from '../components/AddItemFloatButton'
 import type { TimeRange } from '../components/TimeRangePicker'
 import { TimeRangePicker } from '../components/TimeRangePicker'
-import { Topnav } from '../components/Topnav'
+import { TopMenu } from '../components/TopMenu'
+import { TopNav } from '../components/TopNav'
+import { useMenuStore } from '../stores/useMenuStore'
 import { ItemsList } from './ItemsPage/ItemsList'
 import { ItemsSummary } from './ItemsPage/ItemsSummary'
 
@@ -34,15 +36,20 @@ export const ItemsPage: React.FC = () => {
       updated_at: '2021-01-01T00:00:00.000Z',
     }
   ])
+  const { visible, setVisible } = useMenuStore()
+  // false => true
+  // visible: true ; maskVisible 不存在
+
   return (
     <div>
       <Div>
-        <Topnav />
+        <TopNav />
         <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
       </Div>
       <ItemsSummary />
       <ItemsList items={items} />
       <AddItemFloatButton />
+      <TopMenu visible={visible} onClickMask={() => { setVisible(false) }} />
     </div>
   )
 }
